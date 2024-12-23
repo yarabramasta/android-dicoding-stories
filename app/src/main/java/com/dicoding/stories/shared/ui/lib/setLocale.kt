8f3f24c374c5555781d28cf19b400dbe96da9daf
yarabramasta <yarabram111@gmail.com>
2024-12-23 08:20:02 +0700

    fix(l10n): try restart the activity on locale change (not tested yet!)

--- app/src/main/java/com/dicoding/stories/shared/ui/lib/setLocale.kt
+++ app/src/main/java/com/dicoding/stories/shared/ui/lib/setLocale.kt
@@ -3,29 +3,29 @@ package com.dicoding.stories.shared.ui.lib
 import android.app.Activity
 import android.app.LocaleManager
 import android.content.Context
-import android.content.Intent
 import android.os.Build
 import android.os.LocaleList
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
-import java.util.Locale
 
 fun setLocale(context: Context, localeTag: String) {
-  context.findActivity()?.runOnUiThread {
-    Locale.setDefault(Locale.forLanguageTag(localeTag))
+//  context.findActivity()?.runOnUiThread {
+//    Locale.setDefault(Locale.forLanguageTag(localeTag))
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-      context
-        .getSystemService(LocaleManager::class.java)
-        .applicationLocales = LocaleList.forLanguageTags(localeTag)
-    } else {
-      AppCompatDelegate.setApplicationLocales(
-        LocaleListCompat.forLanguageTags(localeTag)
-      )
-    }
-
-    context.startActivity(
-      Intent.makeRestartActivityTask((context as Activity).intent?.component)
+  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    context
+      .getSystemService(LocaleManager::class.java)
+      .applicationLocales = LocaleList.forLanguageTags(localeTag)
+  } else {
+    AppCompatDelegate.setApplicationLocales(
+      LocaleListCompat.forLanguageTags(localeTag)
     )
   }
+
+  (context as Activity).recreate()
+
+//    context.startActivity(
+//      Intent.makeRestartActivityTask((context as Activity).intent?.component)
+//    )
+//  }
 }
